54d089d168e06ce0cdd18e07819cda2f88bf2966
Suporte Campainha <suporte@campainha.com.br>
2026-06-29 10:54:25 -0300

    Melhora visual e de UI/UX do site e portal baseados no cookbook do Claude (scrollbars, glows, e hovers)

---
+++ push.bat
@@ -0,0 +1,5 @@
+@echo off
+set GITHUB_TOKEN=
+git add .
+git commit -m "Melhora visual e de UI/UX do site e portal baseados no cookbook do Claude (scrollbars, glows, e hovers)"
+git push origin main
